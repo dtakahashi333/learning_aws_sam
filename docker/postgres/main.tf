acc831fd866070cdf0ac8fc05b1b7cc2fd697085
Daisuke Takahashi <daisuke332003@gmail.com>
2026-04-04 16:22:27 -0500

    refactor(terraform): switch Docker container volumes to mounts for better compatibility

--- docker/postgres/main.tf
+++ docker/postgres/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
   }
 }
@@ -12,13 +13,25 @@ variable "db_password" {
   sensitive = true
 }
 
+# Pull image with platform constraint
 resource "docker_image" "postgres" {
-  name = "postgres:17.9-trixie"
+  name         = "postgres:17.9-trixie"
+  keep_locally = true
+
+  # Forces x86_64 like docker-compose
+  platform = "linux/amd64"
+}
+
+# Named volume (pgdata)
+resource "docker_volume" "pgdata" {
+  name = "pgdata"
 }
 
 resource "docker_container" "db" {
   name  = "sam-postgres"
   image = docker_image.postgres.name
+
+  restart = "always"
 
   ports {
     internal = 5432
@@ -31,8 +44,18 @@ resource "docker_container" "db" {
     "POSTGRES_PASSWORD=${var.db_password}"
   ]
 
-  volumes {
-    host_path      = abspath("${path.module}/init-db.sql")
-    container_path = "/docker-entrypoint-initdb.d/init-db.sql"
+  # Named volume (persistent data)
+  mounts {
+    target = "/var/lib/postgresql/data"
+    source = docker_volume.pgdata.name
+    type   = "volume"
+  }
+
+  # Init script (read-only bind mount)
+  mounts {
+    target    = "/docker-entrypoint-initdb.d/init-db.sql"
+    source    = abspath("${path.module}/init-db.sql")
+    type      = "bind"
+    read_only = true
   }
 }
